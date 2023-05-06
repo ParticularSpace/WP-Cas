05932c6408d3c6f6cb443f44cc65c8a3206626c4
Sam Jones <samejones2018@gmail.com>
2023-05-06 10:43:20 -0700

    updating user class

--- public/js/app.js
+++ public/js/app.js
@@ -12,36 +12,6 @@ $submitBtn.addEventListener('click', async (event) => {
     return alert('Username and password must be provided');
   }
 
-  try {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({username, password}),
-    });
-    const data = await response.json();
-  //  create li html element
-    const $li = document.createElement('li');
-  //  create anchor tag
-    const $a = document.createElement('a');
-//  set href attribute
-    $a.setAttribute('href', `/users/${data.id}`);
-//  set text content
-    $a.textContent = data.username;
-  //  create p tag
-    const $p = document.createElement('p');
-  //  set text content
-    $p.textContent = data.password;
-  //  append anchor tag to li
-    $li.appendChild($a);
-  //  append p tag to li
-    $li.appendChild($p);
-    // append li to ul
-    $userList.appendChild($li);
-    $username.value = '';
-    $password.value = '';
-  } catch (error) {
-    alert(error);
-  }
-
-
+  console.log(username, password);
+  
 });
