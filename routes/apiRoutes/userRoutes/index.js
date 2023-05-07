532206e0ab61379ee0285439e918ec0fd155011c
Sam Jones <samejones2018@gmail.com>
2023-05-07 15:11:39 -0700

    working on routes

--- routes/apiRoutes/userRoutes/index.js
+++ routes/apiRoutes/userRoutes/index.js
@@ -13,4 +13,6 @@ router.post('/', async (req, res) => {
   }
 });
 
+
+
 module.exports = router;
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -4,7 +4,7 @@ const {User} = require('../../models');
 // /users
 // /users  - render all the users
 // /todos - renders all the todos
-router.get('/users', async (req, res) => {
+router.get('/login', async (req, res) => {
   try {
     const usersData = await User.findAll();
     const users = usersData.map(user => user.get({plain: true}));
@@ -18,22 +18,8 @@ router.get('/users', async (req, res) => {
 });
 
 
-router.get('/users/:userId', async (req, res) => {
-  try {
-    const {userId} = req.params;
-    const userData = await User.findByPk(userId);
-    const user = userData.get({plain: true});
-    const settings = {
-      isCool: true,
-      isHungry: false,
-    };
-    res.render('user_profile', {
-      user,
-      settings,
-    });
-  } catch (error) {
-    res.status(500).json({error});
-  }
+router.get('/home', async (req, res) => {
+ 
 });
 
 
