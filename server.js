e4cdd03fe13bf5c196170737cc48b15bef9c9aac
Sam Jones <samejones2018@gmail.com>
2023-05-06 21:35:07 -0700

    added in new requiered modules to handle cookies also setting up verification and database

--- server.js
+++ server.js
@@ -2,36 +2,48 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const routes = require('./routes');
 const hbs = exphbs.create({});
-
 const sequelize = require('./config/connection');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-// setup express so that it knows we're using handlebars as our
-// template engine
-
-
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// Express middleware
+
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  session({
+    key: 'user_sid',
+    secret: process.env.SESSION_SECRET, 
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 600000,
+    },
+  })
+);
 
 
+app.use((req, res, next) => {
+  if (req.cookies.user_sid && !req.session.user) {
+    res.clearCookie('user_sid');
+  }
+  next();
+});
 
 app.use(routes);
 
-
-
-
-sequelize.sync({force: false}).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
   });
 });
-
