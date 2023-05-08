e1fc61687c8945c967db815708f645dd279459e7
Sam Jones <samejones2018@gmail.com>
2023-05-07 17:54:36 -0700

    updated routes and links

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,26 +1,28 @@
 const router = require('express').Router();
-const {User} = require('../../models');
 
-// /users
-// /users  - render all the users
-// /todos - renders all the todos
-router.get('/login', async (req, res) => {
-  try {
-    const usersData = await User.findAll();
-    const users = usersData.map(user => user.get({plain: true}));
-    res.render('users', {
-      sentence: 'This is a sentence',
-      users
-    });
-  } catch (error) {
-    res.status(500).json({error});
-  }
+// Route for the home page
+router.get('/', (req, res) => {
+  res.render('index');
 });
 
-
-router.get('/home', async (req, res) => {
- 
+// Route for the login page
+router.get('/login', (req, res) => {
+  res.render('login');
 });
 
+// Route for the register page
+router.get('/register', (req, res) => {
+  res.render('register');
+});
+
+// Route for the blackjack page
+router.get('/blackjack', (req, res) => {
+  res.render('blackjack');
+});
+
+// Route for any other non-API routes (catch-all)
+router.get('*', (req, res) => {
+  res.render('404');
+});
 
 module.exports = router;
