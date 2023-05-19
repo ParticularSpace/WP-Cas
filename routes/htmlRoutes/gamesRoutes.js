0baa367011fb212c3f42aa28d4db775c82e2298e
Sam Jones <samejones2018@gmail.com>
2023-05-18 17:02:45 -0700

    logged in nav bar now appears on games

--- routes/htmlRoutes/gamesRoutes.js
+++ routes/htmlRoutes/gamesRoutes.js
@@ -1,29 +1,33 @@
 const router = require('express').Router();
-const { Wallet } = require('../../models');
-const withAuth = require('../../utils/auth'); 
-
-router.get('/games', withAuth, (req, res) => {
-    res.render('games');
-});
+const { Wallet, User } = require('../../models'); // Include User model
+const withAuth = require('../../utils/auth');
 
 router.get('/games', withAuth, async (req, res) => {
-   try{
-        const userData = await User.findByPk(req.session.user_id, {
-            include: {
-                model: Wallet,
-                attributes: ['balance']
-            }
-        });
-
-        const walletBalance = userData.wallet.balance;
-
-        res.render('fundz', {walletBalance});
-
-   }
-   catch (err) {
-    res.status(500).json({ message: 'Error retrieving wallet balance' });
-   }
+  try {
+    // Find the wallet data
+    const walletData = await Wallet.findOne({
+      where: { user_id: req.session.user.id },
+    });
     
+    const wallet = walletData.get({ plain: true });
+
+    // Find the user data
+    const userData = await User.findByPk(req.session.user.id);
+    const user = userData.get({ plain: true });
+
+    // Render 'games' with user details and wallet balance
+    res.render('games', {
+      logged_in: req.session.logged_in, 
+      profilePicture: user.profile_picture, 
+      walletBalance: wallet.balance,
+      username: user.username,
+      
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Error retrieving wallet balance' });
+  }
 });
 
 module.exports = router;
