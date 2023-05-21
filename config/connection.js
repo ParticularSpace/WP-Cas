2af3368ca115ad0cc1da711da6849851bad74a6c
Sam Jones <samejones2018@gmail.com>
2023-05-21 16:54:35 -0700

    herkou merge

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,31 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize'); 
 
 // Create a new Sequelize instance with database configuration
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER, 
-  process.env.DB_PASSWORD, 
-  {
-    host: process.env.DB_HOST, 
-    dialect: process.env.DB_DIALECT, 
-  }
-);
+
+const sequelize = process.env.DATABASE_URL
+    ? new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    })
+    : new Sequelize(database, username, password, config);
+
+
+
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER, 
+//   process.env.DB_PASSWORD, 
+//   {
+//     host: process.env.DB_HOST, 
+//     dialect: process.env.DB_DIALECT, 
+//   }
+// );
 
 module.exports = sequelize;
