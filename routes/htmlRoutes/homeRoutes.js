c2d38c282ce305f10b798137de428d3ab30f645f
Sam Jones <samejones2018@gmail.com>
2023-05-21 17:29:10 -0700

    added landing route

--- routes/htmlRoutes/homeRoutes.js
+++ routes/htmlRoutes/homeRoutes.js
@@ -3,8 +3,19 @@ const router = express.Router();
 const withAuth = require('../../utils/auth');
 const { User } = require('../../models');
 
-router.get('/', async (req, res) => {
-  res.render('dashboard');
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user.id);
+    const user = userData.get({ plain: true });
+    res.render('dashboard', {
+      ...user,
+      logged_in: req.session.logged_in,
+      showNav: true,
+      showCoin: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // Login Page
