c9a50c667e7e0589e2880219d9fd1645faa32329
AlbusSeverus <ericsonhnc@gmail.com>
2023-05-22 22:05:38 -0700

    heroku

--- config/connection.js
+++ config/connection.js
@@ -4,30 +4,30 @@ const { Sequelize } = require('sequelize');
 
 // Create a new Sequelize instance with database configuration for Heroku deployment
 
-// const sequelize = process.env.DATABASE_URL
-//     ? new Sequelize(process.env.DATABASE_URL, {
-//         dialect: 'postgres',
-//         protocol: 'postgres',
-//         dialectOptions: {
-//             ssl: {
-//                 require: true,
-//                 rejectUnauthorized: false
-//             }
-//         }
-//     })
-//     : new Sequelize(database, username, password, config);
+const sequelize = process.env.DATABASE_URL
+    ? new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    })
+    : new Sequelize(database, username, password, config);
 
 
 // Create a new Sequelize instance with database configuration for local deployment
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER, 
-  process.env.DB_PASSWORD, 
-  {
-    host: process.env.DB_HOST, 
-    dialect: process.env.DB_DIALECT, 
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER, 
+//   process.env.DB_PASSWORD, 
+//   {
+//     host: process.env.DB_HOST, 
+//     dialect: process.env.DB_DIALECT, 
+//   }
+// );
 
 module.exports = sequelize;
